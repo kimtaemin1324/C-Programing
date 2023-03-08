9f61ba8f84088af62da653ccb828342905163cf4
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-03-08 12:21:30 +0900

    Add files via upload

---
+++ 20230308_3/20230308_3/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1 X 2 = 2\n");
+	printf("1 X 3 = 3\n");
+	printf("1 X 4 = 4\n");
+	printf("1 X 5 = 5\n");
+	printf("1 X 6 = 6\n");
+	printf("1 X 7 = 7\n");
+	printf("1 X 8 = 8\n");
+	printf("1 X 9 = 9\n");
+	
+
+
+
+	return 0;
+}
