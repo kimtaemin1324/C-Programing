61fca33cb6d178e7ae2aa0fae3fb34f90e1d3f2b
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-04-14 17:41:55 +0900

    Add files via upload

---
+++ c언어 도전 150제/20230414/20230414_13/20230414_13/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	double d = 3.141592;
+	printf("%f\n", d);
+}
---
+++ c언어 도전 150제/20230414/20230414_15/20230414_15/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+	const char* str = "¥Î«—πŒ±π";
+
+	printf("%s\n\n", str);
+	printf("str = %s\n, str");
+}
