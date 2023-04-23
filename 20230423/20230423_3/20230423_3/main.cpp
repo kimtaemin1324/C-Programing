719d36aea8b4b9689e06fc6f35ccc10560dd451d
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-04-24 01:43:30 +0900

    Add files via upload

---
+++ 20230423/20230423_3/20230423_3/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n, sum;
+
+	printf("정수를 입력하시오: ");
+	scanf("%d", &n);
+
+	i = 1;
+	sum = 0;
+
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("1부터 %d 까지의 합은 %d입니다\n", n, sum);
+	return 0;
+
+
+
+}
