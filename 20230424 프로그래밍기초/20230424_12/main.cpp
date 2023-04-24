fccc5b51366fb8159c9c670db69fcbbda942747e
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-04-24 20:12:51 +0900

    Add files via upload

---
+++ 20230424 프로그래밍기초/20230424_12/main.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c, n;
+
+    printf("몇번째 항까지 구할까요? ");
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d, ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
