454745821c7e82e2ce45e4bdb8dbecf5535577ba
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-04-24 22:02:24 +0900

    Add files via upload

---
+++ 20230424/20230424_12/main.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	int n;
+	do {
+		printf("¸·´ëÀÇ ³ôÀÌ(Á¾·á: -1): ");
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	} while (n != -1);
+}
---
+++ 20230424/20230424_13/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	int sum = 0;
+	int i = 0;
+
+	while (1)
+	{
+		sum += i;
+		if (sum > 10000)
+			break;
+		i++;
+	}
+	printf("1ºÎÅÍ %d±îÁöÀÇ ÇÕÀÌ %dÀÔ´Ï´Ù.\n", i - 1, sum - i);
+}
