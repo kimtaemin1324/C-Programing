d1c4e53c1fc718f4d62343296d3845b398b31818
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-09-04 16:45:55 +0900

    Add files via upload

---
+++ 20230904/20230904_6/20230904_6/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int year = 2021;
+	int old = 21;
+	FILE* fp;
+	fp = fopen("Sunday.txt", "w");
+	fprintf(fp, "This year is %d\nMy old is: %d", year, old);
+	fclose(fp);
+}
