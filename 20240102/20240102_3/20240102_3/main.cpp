b1f625ae0b9e604d62857266e8121a0167260d49
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2024-01-03 12:12:40 +0900

    Add files via upload

---
+++ 20240102/20240102_3/20240102_3/main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 2; i <= 10; i++) {
+        c = a + b;
+        printf("%d %d %d\n", a, b, c);
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
